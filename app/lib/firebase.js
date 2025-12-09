1081b77f0b315809e7c59dea103ea3b8b25f70fe
cyberxdevsofficial <cyberxdevsofficial@gmail.com>
2025-12-09 21:48:56 +0530

    Create firebase.js

---
+++ app/lib/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2OhMnrIgbR1GvuINihs28XyACEAYgm3Y",
+  authDomain: "apihub-22441.firebaseapp.com",
+  projectId: "apihub-22441",
+  storageBucket: "apihub-22441.firebasestorage.app",
+  messagingSenderId: "664309433102",
+  appId: "1:664309433102:web:4a3b13bdf1137c17b64d5f",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
